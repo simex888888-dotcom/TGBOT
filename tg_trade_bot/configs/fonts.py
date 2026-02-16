bedf455075042a7768e03c37c1a51fce8c8c311d
simex888888-dotcom <simex888888@gmail.com>
2026-02-16 17:22:07 +0100

    Add files via upload

---
+++ tg_trade_bot/configs/fonts.py
@@ -0,0 +1,68 @@
+FONTS = {
+    "bybit": {
+        "files": {
+            "regular": "fonts/SF_Pro_Display_Regular.otf",
+            "bold": "fonts/SF_Pro_Display_Semibold.otf",
+        },
+        "sizes": {
+            "symbol": 54,
+            "pnl": 42,
+            "leverage": 22,
+            "qty": 37,
+            "entry": 37,
+            "mark": 37,
+            "liq": 37,
+            "badge": 42,
+        },
+        "badge_style": "outline",
+    },
+    "bingx": {
+        "files": {
+            "regular": "fonts/SF_Pro_Display_Regular.otf",
+            "bold": "fonts/SF_Pro_Display_Semibold.otf",
+        },
+        "sizes": {
+            "symbol": 54,
+            "pnl": 54,
+            "leverage": 54,
+            "qty": 52,
+            "entry": 52,
+            "mark": 52,
+            "liq": 52,
+            "badge": 26,
+        },
+        "badge_style": "filled",
+    },
+    "custom_bybit": {
+        "files": {
+            "regular": "fonts/SF_Pro_Display_Regular.otf",
+            "bold": "fonts/SF_Pro_Display_Semibold.otf",
+        },
+        "sizes": {
+            "username": 36,
+            "symbol": 58,
+            "pnl": 120,
+            "entry": 48,
+            "exit": 48,
+            "leverage_text": 40,
+        },
+        "badge_style": "outline",
+    },
+    "custom_bingx": {  # 🆕 BINGX КАСТОМ - БОЛЬШЕ PnL!
+        "files": {
+            "regular": "fonts/SF_Pro_Display_Regular.otf",
+            "bold": "fonts/SF_Pro_Display_Semibold.otf",
+        },
+        "sizes": {
+            "username": 50,      # Меньше username
+            "symbol": 66,        # Как bingx symbol
+            "pnl": 200,          # ОГРОМНЫЙ PnL!
+            "entry": 54,         # Чуть меньше Bybit
+            "exit": 54,          # Чуть меньше Bybit
+            "leverage_text": 66,
+            "side_text":66 # Больше текста плеча
+        },
+        "badge_style": "filled",  # Как bingx стиль
+    }
+}
+
